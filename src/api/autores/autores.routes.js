32e4d1f2d5bd395048305b63e6a7f7369325d552
GaleGizmo <juanmiguelabelleira@gmail.com>
2023-03-07 19:32:18 +0100

    inicio2

--- src/api/autores/autores.routes.js
+++ src/api/autores/autores.routes.js
@@ -1,5 +1,5 @@
 const autoresRoutes = require("express").Router();
-const { isAuth } = require("../../middlewares/auth");
+const { isAuth, isAdmin } = require("../../middlewares/auth");
 const { postAutor, getAutores } = require("./autores.controller");
 
 autoresRoutes.post("/",[isAdmin], postAutor);
